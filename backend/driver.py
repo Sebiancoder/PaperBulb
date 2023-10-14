d55b17ceaba7c87b2623d62eaaf217258a0cf64d
Jaskowski, Sebastian Patrick <jaskowskiseb@gmail.com>
2023-10-13 20:25:13 -0400

    created driver.py
    created driver.py

---
+++ backend/driver.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, Response
+
+app = Flask(__name)
+
+def driver(next_handler):
+    def drive(request):
+        
+        #before handling
+        response = next_handler(request)
+
+        #after handling
+        return response
+
+    return middleware
+
+# Register the middleware with the app
+app.wsgi_app = drive(app.wsgi_app)
+
+# Define your routes
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run()
