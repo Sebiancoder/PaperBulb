913766767e49aeab5243cb1be1b05a8095f66f36
NMBridges <nolanmbridges@gmail.com>
2023-10-13 21:52:47 -0400

    separated

--- backend/semantic_scholar_caller/caller.py
+++ backend/semantic_scholar_caller/caller.py
@@ -1,15 +1,7 @@
-from secrets.secrets import OPENAI, SEMSCHO
+from secrets.secrets import SEMSCHO
 import os
 import openai
 import requests
-
-OAI = False
-
-if OAI:
-    openai.organization = "org-zVepG3kIxRXl8x7hB7Ro2zKF"
-    openai.api_key = f"{OPENAI}"
-    x = openai.Model.list()
-    breakpoint()
 
 SS = False
 
