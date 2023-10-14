e4ebf0d1c25b0d23ac82f047b09f5f39c1c374b0
Ian Knight <ianjarrattknight@gmail.com>
2023-10-14 10:57:34 -0400

    Adding initial node fetch

--- frontend/src/Flow.js
+++ frontend/src/Flow.js
@@ -1,24 +1,69 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import ReactFlow, { ReactFlowProvider, useNodesState, useEdgesState, MiniMap, Controls, Background } from 'react-flow-renderer';
 import { ArticleNode } from './ArticleNode';
 import TextBoxNode from './TextBoxNode';
+import sendBackendRequest from './sendBackendRequest';
 
 const nodeTypes = {
   article: ArticleNode,
   textbox: TextBoxNode
 };
 
-const initialNodes = [
-  { id: '1', type: 'article', position: { x: 0, y: 100 }, data: { label: 'Deep Learning with Images of Dogs' } },
-  { id: '2', type: 'article', position: { x: 300, y: 100 }, data: { label: 'Dogs for Dogs: a Systematic Review' } }
-];
-
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2', type: 'simplebezier' }];
-
 function FlowComponent({ onNodeClick, paperId }) {
-  console.log("paperId:", paperId);
-  const [nodes, setNodes] = useNodesState(initialNodes);
-  const [edges, setEdges] = useEdgesState(initialEdges);
+  const [nodes, setNodes] = useNodesState([]);
+  const [edges, setEdges] = useEdgesState([]);
+
+  const getInitialNodes = async () => {
+    try {
+        const params = new URLSearchParams({ 
+          start_paper: paperId,
+          ref_dlim: 3, // Example depth limit
+          cb_dlim: 2  // Example depth limit for 'cited by'
+        });
+        console.log("params:", params.toString());
+        const response = await sendBackendRequest("generate_graph", params.toString());
+          
+        if(response) {
+            const nodes = [];
+            const edges = [];
+            
+            Object.keys(response).forEach(paperId => {
+                const paper = response[paperId];
+                nodes.push({
+                    id: paperId,
+                    type: 'article',
+                    position: { x: Math.random() * 400, y: Math.random() * 400 }, // random position as an example
+                    data: { label: paper.title || 'No title' }
+                });
+
+                (paper.references || []).forEach(ref => {
+                    if (response[ref]) {  // Check if the referenced paper is in the response
+                        edges.push({
+                            id: `${paperId}-${ref}`,
+                            source: paperId,
+                            target: ref,
+                            type: 'simplebezier'
+                        });
+                    }
+                });
+            });
+
+            setNodes(nodes);
+            setEdges(edges);
+        }
+
+        console.log("Transformed nodes and edges:", nodes, edges);
+    } catch (error) {
+        console.error("Error fetching initial nodes:", error);
+    }
+};
+
+
+  useEffect(() => {
+    if (paperId) {
+      getInitialNodes();
+    }
+  }, [paperId]);
 
   const handleNodeClick = useCallback((event, node) => {
     if (node.type === 'article') {
