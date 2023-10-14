7376375456bbc5881040d4bd39e3d51a39ea0d22
NMBridges <nolanmbridges@gmail.com>
2023-10-13 21:02:21 -0400

    Update oper.py

--- backend/oper.py
+++ backend/oper.py
@@ -3,5 +3,5 @@ import os
 import openai
 
 openai.organization = "org-zVepG3kIxRXl8x7hB7Ro2zKF"
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv(OPENAI)
 openai.Model.list()
