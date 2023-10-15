e28f0af5d84778b0ef583e015eadc7039f0fd5e6
Ian Knight <ianjarrattknight@gmail.com>
2023-10-14 22:11:34 -0400

    node transparency and centering

--- frontend/src/ArticleNode.js
+++ frontend/src/ArticleNode.js
@@ -1,16 +1,27 @@
+import React from 'react';
 import { Handle } from 'react-flow-renderer';
 
 function ArticleNode({ data }) {
-    return (
-    <div style={{ padding: '10px', borderRadius: '5px', background: 'lightgray', border: '1px solid gray' }}>
-        <div style={{ position: 'absolute', top: '50%', transform: 'translateY(-50%)' }}>
-        {/* This is the handle for connections */}
-        <Handle type="target" position="left" style={{ background: '#555' }} />
-        <Handle type="source" position="right" style={{ background: '#555' }} />
-        </div>
-        {data.label}
+  return (
+    <div
+      style={{
+        position: 'relative',
+        padding: '10px',
+        borderRadius: '5px',
+        background: 'lightgray',
+        border: '1px solid gray',
+      }}
+    >
+      {/* Centered handles */}
+      <div style={{ position: 'absolute', top: '50%', left: '0', transform: 'translate(-50%, -50%)' }}>
+        <Handle type="target" position="left" style={{ background: 'transparent' }} />
+      </div>
+      <div style={{ position: 'absolute', top: '50%', right: '0', transform: 'translate(50%, -50%)' }}>
+        <Handle type="source" position="right" style={{ background: 'transparent' }} />
+      </div>
+      {data.label}
     </div>
-    );
+  );
 }
-  
+
 export { ArticleNode };
