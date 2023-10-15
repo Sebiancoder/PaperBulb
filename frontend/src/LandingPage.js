3bc6be5aed52c54582c2810fefc4c8abcac1044d
Ian Knight <ianjarrattknight@gmail.com>
2023-10-14 20:31:02 -0400

    landing page update

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Search from './Search';
 import './LandingPage.css';
 
 const LandingPage = ({ handleEnterSite, papers, setPapers }) => {
-    console.log("LandingPage's papers:", papers);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 10);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const renderPapers = () => {
+        if (Object.keys(papers).length === 0) {
+            return <div className="no-papers-message">Search something...</div>;
+        } else {
+            return Object.entries(papers).map(([paperId, paperMetadata], index) => (
+                <div key={paperId} className="paper" onClick={() => handleEnterSite(paperId)}>
+                    <h2>{paperMetadata.title}</h2>
+                    <p>{paperMetadata.authors[0]["name"]}</p>
+                </div>
+            ));
+        }
+    };
 
     return (
-      <div>
-        <div className='search-container'>
-          <Search setPapers={setPapers} />
+      <div className={isScrolled ? 'scrolled' : ''}>
+        <div className="top-bar">
+            <div className='search-container'>
+                <Search setPapers={setPapers} />
+            </div>
         </div>
         <div className="papers-grid">
-          {Object.entries(papers).map(([paperId, paperMetadata], index) => (
-            <div key={paperId} className="paper" onClick={() => handleEnterSite(paperId)}>
-              <h2>{paperMetadata.title}</h2>
-              <p>{paperMetadata.authors[0]["name"]}</p>
-            </div>
-          ))}
+            {renderPapers()}
         </div>
       </div>
     );
--- frontend/src/Search.js
+++ frontend/src/Search.js
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import { Form, FormControl, Button, Spinner } from 'react-bootstrap';
 import sendBackendRequest from './sendBackendRequest.js';
 
 function Search({ setPapers, setIsLandingPage }) {
     const [searchText, setSearchText] = useState('CNN');
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSearch = async (event) => {
         event.preventDefault();
+        setIsLoading(true);
         console.log(`Searching for ${searchText}...`);
     
         try {
             const params = new URLSearchParams({ query: searchText });
             console.log("params:", params.toString());
-            const papersResponse = await sendBackendRequest("search_papers", params.toString()); // This line fetches the papers.
+            const papersResponse = await sendBackendRequest("search_papers", params.toString()); 
             if (typeof setIsLandingPage === "function") {
                 setIsLandingPage(true);
             }
             if (papersResponse) {
-                setPapers(papersResponse);  // This sets the papers in the parent component.
+                setPapers(papersResponse); 
             }
         } catch (error) {
             console.error("Error fetching papers:", error);
+        } finally {
+            setIsLoading(false);
         }
-    
     };
 
     return (
@@ -34,8 +37,11 @@ function Search({ setPapers, setIsLandingPage }) {
                     className="mr-sm-2 search-input"
                     value={searchText}
                     onChange={(event) => setSearchText(event.target.value)}
+                    disabled={isLoading}
                 />
-                <Button variant="outline-success" type="submit" className="search-button">Search</Button>
+                <Button variant="outline-success" type="submit" className="search-button" disabled={isLoading}>
+                    {isLoading ? <Spinner animation="border" size="sm" /> : 'Search'}
+                </Button>
             </div>
         </Form>
     );
