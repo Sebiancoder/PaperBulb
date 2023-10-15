f9ac2f3e92c033c2b19e64b0094f17430bb412a0
Ian Knight <ianjarrattknight@gmail.com>
2023-10-15 04:23:59 -0400

    final for now

--- frontend/src/LandingPage.js
+++ frontend/src/LandingPage.js
@@ -6,15 +6,7 @@ import paperbulb from './paperbulb.svg';
 const LandingPage = ({ handleEnterSite, papers, setPapers }) => {
     const renderPapers = () => {
         if (Object.keys(papers).length === 0) {
-            return (<div className="no-papers-message">
-            
-            <p className="info-text">           
-            <img src={paperbulb} className="logo" alt="PaperBulb logo"/> is a learning tool for budding researchers. Focused on making literature review approachable, PaperBulb 
-            displays the lineage of an idea and the context of a paper in a graph timeline. It simplifies the process of
-            finding relevant papers, understanding their content, and discovering new ideas.
-            Search for a paper to get started!
-            </p>
-        </div>)
+            return null; // return null if you don't want to display any papers
         } else {
             return Object.entries(papers).map(([paperId, paperMetadata], index) => (
                 <div key={paperId} className="paper" onClick={() => handleEnterSite(paperId)}>
@@ -32,6 +24,18 @@ const LandingPage = ({ handleEnterSite, papers, setPapers }) => {
                 <Search setPapers={setPapers} />
             </div>
         </div>
+
+        {Object.keys(papers).length === 0 && (
+                <div className="no-papers-message">
+                    <p className="info-text">           
+                        <img src={paperbulb} className="logo" alt="PaperBulb logo"/> is a learning tool for budding researchers. Focused on making literature review approachable, PaperBulb 
+                        displays the lineage of an idea and the context of a paper in a graph timeline. It simplifies the process of
+                        finding relevant papers, understanding their content, and discovering new ideas.
+                        Search for a paper to get started!
+                    </p>
+                </div>
+            )}
+
         <div className="papers-grid">
             {renderPapers()}
         </div>
@@ -40,3 +44,5 @@ const LandingPage = ({ handleEnterSite, papers, setPapers }) => {
 };
 
 export default LandingPage;
+
+
